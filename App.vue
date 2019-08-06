e6fac88b3fa88712be78dbb251fcdd5bc5afd408
mauriciopul <mauriciopulhotmail.com>
2019-08-06 09:45:00 -0500

    Comienzo de proyecto front con vue para el carrito de compras de Software Estraategico

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <h1>{{car}}</h1>
+  <table>
+    <body>
+      <tr>
+        <td>Hola1</td>
+        <td>Hola2</td>
+        <td>Hola3</td>
+      </tr>
+    </body>
+  </table>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      car: "Hola mundo!!!"
+    };
+  },
+  mounted() {
+    console.log("hola mundo mounted");
+  }
+};
+</script>
+
+<style>
+</style>
