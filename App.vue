27d7decf826675e06b0e455612731bdea2f0abc5
mauriciopul <mauriciopulhotmail.com>
2019-08-06 16:27:47 -0500

    Guarda registros pero solo con el dato de precio 'quemado'

--- App.vue
+++ App.vue
@@ -1,14 +1,23 @@
 <template>
-<div>
-  <table>
-    <body>
-      <tr v-for="new_post in news" v-bind:key="new_post.id">
-        <td>{{new_post.id}}</td>
-        <td>{{new_post.title}}</td>
-      </tr>
-    </body>
-  </table>
-</div>
+  <div>
+    <div>
+      <form>
+        <input v-model="codigoProducto" placeholder="Codigo del producto" />
+        <input v-model="nombreProducto" placeholder="Nombre del producto" />
+        <input v-model="precioProducto" placeholder="Precio del producto" />
+      </form>
+      <button v-on:click="postProducts()">Crear</button>
+    </div>
+    <table>
+      <tbody>
+        <tr v-for="product_post in products" :key="product_post.codigoProducto">
+          <td>{{product_post.codigoProducto}}</td>
+          <td>{{product_post.nombreProducto}}</td>
+          <td>{{product_post.precioProducto}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -17,21 +26,46 @@ import axios from "axios";
 export default {
   data() {
     return {
-      news: ""
+      products: "",
+      codigoProducto: "",
+      nombreProducto: "",
+      precioProducto: 2
     };
   },
 
   mounted() {
-    console.log("hola mundo mounted");
-    this.getNews();
+    this.getProducs();
   },
 
   methods: {
-    getNews() {
-      axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
-        this.news = response.data;
-        console.log("Data de response", response);
-      });
+    getProducs() {
+      axios
+        .get("http://localhost:3000/products/")
+        .then(response => {
+          this.products = response.data.products;
+          console.log("Data de response", response.data.products);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    postProducts() {
+      const params = {
+        codigoProducto: this.codigoProducto,
+        nombreProducto: this.nombreProducto,
+        precioProducto: this.precioProducto
+      };
+      axios
+        .post("http://localhost:3000/products/", params)
+        .then(response => {
+          this.codigoProducto = "";
+          this.nombreProducto = "";
+          this.precioProducto;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      this.getProducs();
     }
   }
 };
