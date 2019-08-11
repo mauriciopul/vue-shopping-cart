ef30225eab61db015d36b4bc2acea5c146a039ba
Mauricio Pulgarin <mauriciopul@gmail.com>
2019-08-10 21:59:54 -0500

    crear usuarios desde el pc de casa

--- App.vue
+++ App.vue
@@ -11,10 +11,20 @@
     </div>
     <table>
       <tbody>
-        <tr v-for="product_post in products" :key="product_post.codigoProducto">
-          <td>{{product_post.codigoProducto}}</td>
-          <td>{{product_post.nombreProducto}}</td>
+        <h2>
+          </h2>
+            <tr>
+            <td><h2>id</h2></td>
+            <td><h2>Producto</h2></td>
+            <td><h2>Precio</h2></td>
+       
+          </tr>
+        
+          <tr v-for="product_post in products" :key="product_post.codigoProducto">
+          <td><h4>{{product_post.codigoProducto}}</h4></td>          
+          <td>{{product_post.nombreProducto}}</td>          
           <td>{{product_post.precioProducto}}</td>
+          
         </tr>
       </tbody>
     </table>
@@ -36,6 +46,8 @@ export default {
 
   mounted() {
     this.getProducs();
+    //this.created();
+    //axios.create();
   },
 
   methods: {
@@ -55,17 +67,28 @@ export default {
         nombreProducto: this.nombreProducto,
         precioProducto: parseInt(this.precioProducto)
       };
+
       axios
         .post("http://localhost:3000/products/", params)
         .then(response => {
           this.codigoProducto = "";
           this.nombreProducto = "";
           this.precioProducto;
+          this.getProducs();
         })
         .catch(error => {
           console.log(error);
         });
-      this.getProducs();
+    },
+    getOneProduct(codigoProducto) {
+      axios
+        .get("http://localhost:3000/products/${codigoProducto}")
+        .then(response => {
+          console.log(response.data);
+          this.codigoProducto = response.data.codigoProducto;
+          this.nombreProducto = response.data.nombreProducto;
+          this.precioProducto = response.data.precioProducto;
+        });
     }
   }
 };
